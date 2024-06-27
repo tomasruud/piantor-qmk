675b3f1c7f9c22bcbd0aa6adf97d895b042c6e35
Tomas Ruud <tru@axogroup.com>
2024-06-27 15:55:46 +0200

    Add rendered keymap

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ qmk_map := $(qmk_dir)/keyboards/beekeeb/piantor_pro/keymaps/tomasruud
 
 all: $(bin_name)
 
-$(bin_name):
+$(bin_name): keymap.txt
 	git submodule update --init --recursive
 	rm -rfd $(qmk_map)
 	cp -r ./src $(qmk_map)
@@ -15,6 +15,10 @@ $(bin_name):
 	mv $(qmk_dir)/.build/$(bin_name) out/.
 	rm -rfd $(qmk_map)
 
+keymap.txt:
+	go run gen_keymap.go < src/keymap.c > keymap.txt
+
 clean:
+	rm keymap.txt
 	rm -rfd $(qmk_map)
 	rm -f ./bin/$(bin_name)
---
+++ gen_keymap.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	in, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	lns := strings.Split(string(in), "\n")
+
+	for i := 0; i < len(lns); i++ {
+		if !strings.Contains(lns[i], "const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS]") {
+			continue
+		}
+		i++
+
+		for i < len(lns) {
+			ln := lns[i]
+			tln := strings.TrimSpace(ln)
+
+			if strings.Contains(ln, "}") {
+				os.Exit(0)
+			}
+
+			if strings.HasPrefix(tln, "//") {
+				fmt.Println(ln[7:])
+			} else if strings.HasPrefix(tln, "[") {
+				fmt.Println("Layer " + ln[5:8])
+			} else if strings.HasPrefix(tln, ")") {
+			} else {
+				fmt.Println(ln[7:])
+			}
+
+			i++
+		}
+	}
+}
